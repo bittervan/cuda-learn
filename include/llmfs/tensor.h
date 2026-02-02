510ced10678a16c609abdf9db09ed286403b7fe1
Xingkai Wang <xingkai.wang@outlook.com>
2026-02-02 14:49:24 +0800

    wrong format, wrong size

--- include/llmfs/tensor.h
+++ include/llmfs/tensor.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <initializer_list>
 
@@ -10,10 +11,16 @@ static inline int numel(const std::vector<int> &shape) {
 }
 
 class Tensor {
-    std::vector<int> shape;
-    std::vector<float> data;
+    std::vector<int> shape_;
+    std::vector<float> data_;
 
 public:
-    Tensor(const std::vector<int> &shape) : shape(shape), data(numel(shape), 0.0f) {}
+    Tensor(const std::vector<int> &shape) : shape_(shape), data_(numel(shape), 0.0f) {}
     Tensor(const std::initializer_list<int> &s) : Tensor(std::vector<int>(s)) {}
+
+    int ndim() const;
+    const std::vector<int>& shape() const;
+    const std::vector<float>& data() const;
 };
+
+std::ostream& operator<<(std::ostream& os, const Tensor& t);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <vector>
 
 #include <llmfs/tensor.h>
 
 int main() {
     std::cout << "hello, world" << std::endl;
     Tensor t{2, 3};
+    std::cout << t;
     return 0;
 }
