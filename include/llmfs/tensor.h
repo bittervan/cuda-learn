9704528d9350d476084b1d5534174703250042e4
Xingkai Wang <xingkai.wang@outlook.com>
2026-02-02 12:55:22 +0800

    sync

--- include/llmfs/tensor.h
+++ include/llmfs/tensor.h
@@ -1,5 +1,17 @@
 #include <vector>
 
+static inline int numel(const std::vector<int> &shape) {
+    int total = 1;
+    for (auto i : shape) {
+        total *= i;
+    }
+    return total;
+}
+
 class Tensor {
+    std::vector<int> shape;
     std::vector<float> data;
+
+public:
+    Tensor(const std::vector<int> &shape) : shape(shape), data(numel(shape), 0.0f) {}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
+
+#include <llmfs/tensor.h>
 
 int main() {
     std::cout << "hello, world" << std::endl;
+    Tensor t({2, 3});
     return 0;
 }
